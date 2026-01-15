83bfb8c742c1bbedbfd6ffaf175c0ef539377857
Aiden Chen <197520251+acstartup@users.noreply.github.com>
2026-01-15 15:38:19 -0500

    feat(address and date and limiter): implemented address and start date input and also 3 bill capacity

--- app/page.tsx
+++ app/page.tsx
@@ -6,35 +6,45 @@ import { saveBill, bill } from '@/lib/deploy';
 export default function Home() {
   const [bills, showBill] = useState<bill[]>([]);  
 
-  // default lines
+  {/* default lines */}
   const [address, setAddress] = useState('');
   const [type, setType] = useState(''); // type = value and setType = function to change it
   const [recurrence, setRecurrence] = useState('');
   const [price, setPrice] = useState('');
   const [description, setDescription] = useState('');
-  const [date, setDate] = useState('');
+  const [date, setDate] = useState(new Date().toISOString().split('T')[0]);
 
-  /* function to handle compute w/ save and note and deploy */
+  {/* function to handle compute w/ save and note and deploy */}
   const handleBill = () => {
     if (!type || !recurrence || !price || !description) {
       alert("All bill information inputs must be filled to add a bill.");
       return;
     }
+  
+    {/* 3 bill limiter */}
+    if (bills.length > 3) {
+      alert("Hit max capacity of 3 bills, delete a current one to add more")
+      return;
+    }
 
     const newBill = {
+      address: address,
       type: type,
       recurrence: recurrence,
       price: parseFloat(price),
-      description: description
+      description: description,
+      date: date
     }
     showBill([...bills, newBill]);
     saveBill(newBill);
 
     {/* clear bill spots */}
+    setAddress("");
     setType("");
     setRecurrence("");
     setPrice("");
     setDescription("");
+    setDate("");
   };
 
   const handleDelete = (indexToDelete: number) => {
@@ -51,7 +61,7 @@ export default function Home() {
           id="address"
           type="text"
           value={address}
-          onChange={(e) => setType(e.target.value)}
+          onChange={(e) => setAddress(e.target.value)}
           className="border border-black outline-black text-black w-50 h-6 px-1 rounded-md bg-gray-100 text-sm"
           placeholder="Address"
         >
@@ -95,7 +105,7 @@ export default function Home() {
           placeholder="Description"
         ></input>
         <input
-          id="date"
+          id="startDate"
           type="date"
           value={date}
           onChange={(e) => setDate(e.target.value)}
@@ -112,12 +122,13 @@ export default function Home() {
 
       {/* 1. return for computed info into a UI computed bill but not yet deployed */}
       {bills.map((bill, index) => (
-      <div key={index} className="relative border bg-slate-50 mb-2 border-black shadow-md rounded-lg text-black text-md px-2 py-1 right-30 top-10 w-81">
+      <div key={index} className="relative border bg-slate-50 mb-2 border-black shadow-md rounded-lg text-black text-md px-2 py-1 right-30 top-10 w-max h-max">
         <div className="flex justify-between">
-          <span className="font-bold text-sm">Your Bill:</span>
+          <span className="font-bold text-sm">{bill.address}</span>
+          <span className="text-xs font-semibold relative right-20 top-0.5 pl-5">{bill.date}</span>
           <a onClick={() => handleDelete(index)} className="relative text-center font-bold border leading-none w-4.5 h-4.5 text-sm rounded-md hover:outline-[0.5]">x</a> {/* gives an index value to handleDelete and delete searches and deletes */}
         </div>
-        <div className="flex gap-8">
+        <div className="flex flex-row gap-4">
           <span className="text-xs"><span className="font-bold">Type:</span> {bill.type}</span>
           <span className="text-xs"><span className="font-bold">Recurrence:</span> {bill.recurrence}</span>
           <span className="text-xs"><span className="font-bold">Price:</span> ${bill.price}</span>
--- lib/deploy.ts
+++ lib/deploy.ts
@@ -1,16 +1,18 @@
 /* TyseScript declaring Bill component types */
 export interface bill {
+    address: string
     type: string
     recurrence: string
     price: number
     description: string
+    date: string
 }
 
 /* function to save bill to localStorage */
 export async function saveBill (newBill: bill) { {/* newBill is a type of bill, bills = array, taking the bills from array then pushing newBill into bills array, then JSON.strinigfy newBill into bills array into localStorage */}
     let bills = JSON.parse(localStorage.getItem('bills') || '[]');
     bills.push(newBill);
-    localStorage.setItem('Bills', JSON.stringify(newBill));
+    localStorage.setItem('bills', JSON.stringify(newBill));
     alert("Bill Saved");
 }
 
